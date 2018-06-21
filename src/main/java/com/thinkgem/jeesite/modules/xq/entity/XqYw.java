a02eb246d53ec66d0dbc08927f94f001a760888b
reehom <13556344950@163.com>
2018-06-21 14:33:53 +0800

    third

---
+++ src/main/java/com/thinkgem/jeesite/modules/xq/entity/XqYw.java
@@ -0,0 +1,97 @@
+/**
+ * Copyright &copy; 2012-2016 <a href="https://github.com/thinkgem/jeesite">JeeSite</a> All rights reserved.
+ */
+package com.thinkgem.jeesite.modules.xq.entity;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.thinkgem.jeesite.common.persistence.DataEntity;
+
+/**
+ * 需求业务表Entity
+ * @author ThinkGem
+ * @version 2018-06-21
+ */
+public class XqYw extends DataEntity<XqYw> {
+	
+	private static final long serialVersionUID = 1L;
+	private String xqId;		// xq_id
+	private String xqTitle;		// xq_title
+	private String xqSsxt;		// 所属系统
+	private String xqXqly;		// 需求来源
+	private String xqXqms;		// 需求描述
+	private String xqXqxh;		// 需求细化
+	private String xqShr;		// 审核人
+	
+	public XqYw() {
+		super();
+	}
+
+	public XqYw(String id){
+		super(id);
+	}
+
+	@Length(min=1, max=64, message="xq_id长度必须介于 1 和 64 之间")
+	public String getXqId() {
+		return xqId;
+	}
+
+	public void setXqId(String xqId) {
+		this.xqId = xqId;
+	}
+	
+	@Length(min=1, max=255, message="xq_title长度必须介于 1 和 255 之间")
+	public String getXqTitle() {
+		return xqTitle;
+	}
+
+	public void setXqTitle(String xqTitle) {
+		this.xqTitle = xqTitle;
+	}
+	
+	@Length(min=1, max=64, message="所属系统长度必须介于 1 和 64 之间")
+	public String getXqSsxt() {
+		return xqSsxt;
+	}
+
+	public void setXqSsxt(String xqSsxt) {
+		this.xqSsxt = xqSsxt;
+	}
+	
+	@Length(min=1, max=255, message="需求来源长度必须介于 1 和 255 之间")
+	public String getXqXqly() {
+		return xqXqly;
+	}
+
+	public void setXqXqly(String xqXqly) {
+		this.xqXqly = xqXqly;
+	}
+	
+	@Length(min=0, max=255, message="需求描述长度必须介于 0 和 255 之间")
+	public String getXqXqms() {
+		return xqXqms;
+	}
+
+	public void setXqXqms(String xqXqms) {
+		this.xqXqms = xqXqms;
+	}
+	
+	@Length(min=0, max=255, message="需求细化长度必须介于 0 和 255 之间")
+	public String getXqXqxh() {
+		return xqXqxh;
+	}
+
+	public void setXqXqxh(String xqXqxh) {
+		this.xqXqxh = xqXqxh;
+	}
+	
+	@Length(min=0, max=64, message="审核人长度必须介于 0 和 64 之间")
+	public String getXqShr() {
+		return xqShr;
+	}
+
+	public void setXqShr(String xqShr) {
+		this.xqShr = xqShr;
+	}
+	
+}
---
+++ src/main/java/com/thinkgem/jeesite/modules/xq/web/XqFjclController.java
@@ -0,0 +1,84 @@
+/**
+ * Copyright &copy; 2012-2016 <a href="https://github.com/thinkgem/jeesite">JeeSite</a> All rights reserved.
+ */
+package com.thinkgem.jeesite.modules.xq.web;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.thinkgem.jeesite.common.config.Global;
+import com.thinkgem.jeesite.common.persistence.Page;
+import com.thinkgem.jeesite.common.web.BaseController;
+
+import com.thinkgem.jeesite.modules.xq.entity.XqFjcl;
+import com.thinkgem.jeesite.modules.xq.service.XqFjclService;
+
+/**
+ * 附件材料表Controller
+ * @author ThinkGem
+ * @version 2018-06-21
+ */
+@Controller
+@RequestMapping(value = "${adminPath}/xq/xqFjcl")
+public class XqFjclController extends BaseController {
+
+	@Autowired
+	private XqFjclService xqFjclService;
+	
+	@ModelAttribute
+	public XqFjcl get(@RequestParam(required=false) String id) {
+		XqFjcl entity = null;
+		if (StringUtils.isNotBlank(id)){
+			entity = xqFjclService.get(id);
+		}
+		if (entity == null){
+			entity = new XqFjcl();
+		}
+		return entity;
+	}
+	
+	@RequiresPermissions("xq:xqFjcl:view")
+	@RequestMapping(value = {"list", ""})
+	public String list(XqFjcl xqFjcl, HttpServletRequest request, HttpServletResponse response, Model model) {
+		Page<XqFjcl> page = xqFjclService.findPage(new Page<XqFjcl>(request, response), xqFjcl); 
+		model.addAttribute("page", page);
+		return "modules/xq/xqFjclList";
+	}
+
+	@RequiresPermissions("xq:xqFjcl:view")
+	@RequestMapping(value = "form")
+	public String form(XqFjcl xqFjcl, Model model) {
+		model.addAttribute("xqFjcl", xqFjcl);
+		return "modules/xq/xqFjclForm";
+	}
+
+	@RequiresPermissions("xq:xqFjcl:edit")
+	@RequestMapping(value = "save")
+	public String save(XqFjcl xqFjcl, Model model, RedirectAttributes redirectAttributes) {
+		if (!beanValidator(model, xqFjcl)){
+			return form(xqFjcl, model);
+		}
+		xqFjclService.save(xqFjcl);
+		addMessage(redirectAttributes, "保存附件成功");
+		return "redirect:"+Global.getAdminPath()+"/xq/xqFjcl/?repage";
+	}
+	
+	@RequiresPermissions("xq:xqFjcl:edit")
+	@RequestMapping(value = "delete")
+	public String delete(XqFjcl xqFjcl, RedirectAttributes redirectAttributes) {
+		xqFjclService.delete(xqFjcl);
+		addMessage(redirectAttributes, "删除附件成功");
+		return "redirect:"+Global.getAdminPath()+"/xq/xqFjcl/?repage";
+	}
+
+}
